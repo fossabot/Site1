48f0a476a92924aa1baf712ceed0f9b8799983ce
Gabriel Aguiar <fgsoftware1@gmail.com>
2020-04-27 20:19:59 +0100

    commit7

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,41 @@
-var gulp = require("gulp");
-var babel = require("gulp-babel");
+var gulp = require('gulp'),
+    watch = require("gulp-watch"),
+    less = require('gulp-less'),
+    cssmin = require('gulp-cssmin'),
+    rename = require('gulp-rename'),
+    babel = require('gulp-babel'),
+    cli = require('gulp-cli');
 
-gulp.task("default", function () {
-  return gulp.src("src/app.js")
-    .pipe(babel())
-    .pipe(gulp.dest("dist"));
+gulp.task('watch-less', function () {
+    watch({glob: './*.less'}, function (files) { 
+        gulp.start('compile-less');
+    });
+
+    watch({
+        glob: ['./*.css', '!./*.min.css']
+    }, function(files) {
+        gulp.start('minify-css');
+    });
 });
+
+gulp.task('compile-less', function () {
+    gulp.src('./*.less')
+    .pipe(less().on('error', function(err) {
+        console.log(err);
+    }))
+    .pipe(gulp.dest('./dist/main.css'));
+});
+
+gulp.task('minify-css', function() {
+    gulp.src([
+        './*.css',
+        '!./*.min.css'
+    ])
+    .pipe(cssmin().on('error', function(err) {
+        console.log(err);
+    }))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./'));
+})
+
+gulp.task('default', ['watch-less']);
